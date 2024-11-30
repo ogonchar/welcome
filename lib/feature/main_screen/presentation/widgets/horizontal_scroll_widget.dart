e05b8737adba78f9019e1b46405d78858e875e0a
Oleg Gonchar <o.gonchar@live.com>
2024-11-30 09:24:23 +0300

    horizontal slider text fix

--- lib/feature/main_screen/presentation/widgets/horizontal_scroll_widget.dart
+++ lib/feature/main_screen/presentation/widgets/horizontal_scroll_widget.dart
@@ -132,13 +132,16 @@ class _HorizontalScrollWidgetItem extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                text,
-                maxLines: 2,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+              SizedBox(
+                width: 150,
+                child: Text(
+                  text,
+                  maxLines: 2,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
               const SizedBox(
